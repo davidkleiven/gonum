46213e7ef6fd5ededc9b4c40b332e6eef500163c
David Kleiven <davidkleiven446@gmail.com>
2021-03-14 21:41:29 +0100

    WIP: Implement mat.Vector interface

--- spatial/qhull/qhull.go
+++ spatial/qhull/qhull.go
@@ -5,9 +5,36 @@ import (
 	"gonum.org/v1/gonum/mat"
 )
 
-// Point represents a point in a Euclidean k-d space that satisfies the Comparable
-// interface.
+// Point represents a point in a Euclidean space. Satisfies the mat.Vector interface.
 type Point []float64
+
+// At returns the element at row i. It panics if i is out of bounds or if j is not zero.
+func (p Point) At(i, j int) float64 {
+	if j != 0 {
+		panic("j must be zero for Point type")
+	}
+	return p[i]
+}
+
+// AtVec returns the element at row i
+func (p Point) AtVec(i int) float64 {
+	return p[i]
+}
+
+// Len returns the length of the vector
+func (p Point) Len() int {
+	return len(p)
+}
+
+// Dims returns the number of rows and columns in the matrix. Columns is always 1.
+func (p Point) Dims() (int, int) {
+	return p.Len(), 1
+}
+
+// T performs an implicit transpose by returning the receiver inside a Transpose.
+func (p Point) T() mat.Matrix {
+	return mat.Transpose{p}
+}
 
 // centroid calculates the centroid of the set of points
 func centroid(points []Point) Point {
@@ -75,11 +102,11 @@ func shareHyperPlane(points []Point, tol float64) bool {
 	c := centroid(points)
 	normal := fitPlane(points)
 	
-	centroidDotNormal := dotPointVector(c, normal)
+	centroidDotNormal := mat.Dot(c, normal)
 	// If all points are perpendicular to the normal, they are in the same plane
 	for _, p := range points {
 		// Want to check if (p - c) * normal = 0, where c is the centroid
-		if math.Abs(dotPointVector(p, normal) - centroidDotNormal) > tol {
+		if math.Abs(mat.Dot(p, normal) - centroidDotNormal) > tol {
 			return false
 		}
 	}
